63c51c6a75259b273dd4d0008b44d062ff3ac8de
ChitrangSChauhan <chitrang1739@gmail.com>
2026-04-30 17:14:35 +0530

    Update Obstacle.kt

--- app/src/main/java/com/example/dodgerush/Obstacle.kt
+++ app/src/main/java/com/example/dodgerush/Obstacle.kt
@@ -1,1 +1,27 @@
-var x = (50..1000).random().toFloat()
+package com.example.dodgerush
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+
+class Obstacle {
+
+    var x = (50..1000).random().toFloat()
+    var y = 0f
+    private val size = 80f
+
+    fun update() {
+        y += 15
+    }
+
+    fun draw(canvas: Canvas) {
+        val paint = Paint()
+        paint.color = Color.RED
+        canvas.drawRect(x, y, x + size, y + size, paint)
+    }
+
+    fun rect(): RectF {
+        return RectF(x, y, x + size, y + size)
+    }
+}
