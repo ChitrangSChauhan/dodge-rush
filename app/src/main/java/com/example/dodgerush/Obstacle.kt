d4525749fc52677a2ac88e554f5befd4f048046a
ChitrangSChauhan <chitrang1739@gmail.com>
2026-04-30 18:16:43 +0530

    Update Obstacle.kt

--- app/src/main/java/com/example/dodgerush/Obstacle.kt
+++ app/src/main/java/com/example/dodgerush/Obstacle.kt
@@ -1,27 +1,37 @@
 package com.example.dodgerush
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
+import kotlin.random.Random
 
 class Obstacle {
 
-    var x = (50..1000).random().toFloat()
+    var lane = (0..2).random()
     var y = 0f
-    private val size = 80f
 
-    fun update() {
-        y += 15
+    private val width = 100f
+    private val height = 160f
+
+    private val colors = listOf(Color.YELLOW, Color.GREEN, Color.BLUE)
+    private val color = colors[Random.nextInt(colors.size)]
+
+    fun update(speed: Float, screenWidth: Int) {
+        y += speed
     }
 
-    fun draw(canvas: Canvas) {
+    fun draw(canvas: Canvas, screenWidth: Int) {
+        val laneWidth = screenWidth / 3f
+        val x = lane * laneWidth + laneWidth / 2 - width / 2
+
         val paint = Paint()
-        paint.color = Color.RED
-        canvas.drawRect(x, y, x + size, y + size, paint)
+        paint.color = color
+
+        canvas.drawRect(x, y, x + width, y + height, paint)
     }
 
-    fun rect(): RectF {
-        return RectF(x, y, x + size, y + size)
+    fun rect(screenWidth: Int): RectF {
+        val laneWidth = screenWidth / 3f
+        val x = lane * laneWidth + laneWidth / 2 - width / 2
+
+        return RectF(x, y, x + width, y + height)
     }
 }
