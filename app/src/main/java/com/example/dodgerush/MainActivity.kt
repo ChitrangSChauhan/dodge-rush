b2cd82c773424217406f26addd35771a08f0f2ce
ChitrangSChauhan <chitrang1739@gmail.com>
2026-04-30 17:14:14 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/dodgerush/MainActivity.kt
+++ app/src/main/java/com/example/dodgerush/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(GameView(this))
+
+        val gameView = GameView(this)
+        setContentView(gameView)
     }
 }
