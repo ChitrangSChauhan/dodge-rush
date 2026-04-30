e3e7e9bfb17be08b97d86e5bcbb351da25019633
ChitrangSChauhan <chitrang1739@gmail.com>
2026-04-30 16:33:01 +0530

    Create AdManager.kt

---
+++ app/src/main/java/com/example/dodgerush/AdManager.kt
@@ -0,0 +1,6 @@
+package com.example.dodgerush
+
+object AdManager {
+    const val BANNER = "ca-app-pub-3940256099942544/6300978111"
+    const val INTERSTITIAL = "ca-app-pub-3940256099942544/1033173712"
+}
