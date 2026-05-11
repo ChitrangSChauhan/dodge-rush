fcc46f4adb643d6dbd9e47e9bac7e4dc3301f04d
ChitrangSChauhan <chitrang1739@gmail.com>
2026-05-11 20:53:32 +0530

    Update Obstacle.kt images

--- app/src/main/java/com/example/dodgerush/Obstacle.kt
+++ app/src/main/java/com/example/dodgerush/Obstacle.kt
@@ -1,34 +1,66 @@
 package com.example.dodgerush
 
-import android.graphics.*
-import kotlin.random.Random
-
 class Obstacle {
 
     var lane = (0..2).random()
-    var y = 0f
+    var y = -220f
 
-    private val width = 100f
-    private val height = 160f
+    private val width = 140f
+    private val height = 220f
 
-    private val colors = listOf(Color.YELLOW, Color.GREEN, Color.BLUE)
-    private val color = colors[Random.nextInt(colors.size)]
+    private val colors = listOf(
+        Color.YELLOW,
+        Color.GREEN,
+        Color.BLUE
+    )
+
+    private val color = colors.random()
 
     fun update(speed: Float, screenWidth: Int) {
         y += speed
     }
 
     fun draw(canvas: Canvas, screenWidth: Int) {
+
         val laneWidth = screenWidth / 3f
         val x = lane * laneWidth + laneWidth / 2 - width / 2
 
         val paint = Paint()
-        paint.color = color
 
-        canvas.drawRect(x, y, x + width, y + height, paint)
+        val rect = RectF(
+            x,
+            y,
+            x + width,
+            y + height
+        )
+
+        paint.color = color
+        canvas.drawRoundRect(rect, 25f, 25f, paint)
+
+        // windshield
+        paint.color = Color.WHITE
+        canvas.drawRoundRect(
+            x + 20,
+            y + 40,
+            x + width - 20,
+            y + 120,
+            15f,
+            15f,
+            paint
+        )
+
+        // wheels
+        paint.color = Color.BLACK
+
+        canvas.drawRect(x - 10, y + 30, x + 10, y + 80, paint)
+        canvas.drawRect(x - 10, y + 140, x + 10, y + 190, paint)
+
+        canvas.drawRect(x + width - 10, y + 30, x + width + 10, y + 80, paint)
+        canvas.drawRect(x + width - 10, y + 140, x + width + 10, y + 190, paint)
     }
 
     fun rect(screenWidth: Int): RectF {
+
         val laneWidth = screenWidth / 3f
         val x = lane * laneWidth + laneWidth / 2 - width / 2
 
